5e0f744028e12b48b87126177467d5c335f5b51c
Aayush Jain <50782898+jain-aayush1123@users.noreply.github.com>
2022-06-11 00:45:52 +0530

    intial code

---
+++ README.md
@@ -0,0 +1,1 @@
+## Decentralized video sharing application published on IPFS
---
+++ src/contracts/DVideo.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
+
+contract DVideo {
+    uint256 public videoCount = 0;
+    string public name = "DVideo";
+
+    //Create id=>struct mapping
+
+    //Create Struct
+
+    //Create Event
+
+    constructor() public {}
+
+    function uploadVideo(string memory _videoHash, string memory _title)
+        public
+    {
+        // Make sure the video hash exists
+        // Make sure video title exists
+        // Make sure uploader address exists
+        // Increment video id
+        // Add video to the contract
+        // Trigger an event
+    }
+}
