d1d79ac51c6f912fbe2a179337afa25b6e512f05
Aayush Jain <50782898+jain-aayush1123@users.noreply.github.com>
2022-06-11 00:56:55 +0530

    add smart contract

--- src/contracts/DVideo.sol
+++ src/contracts/DVideo.sol
@@ -5,22 +5,39 @@ contract DVideo {
     uint256 public videoCount = 0;
     string public name = "DVideo";
 
-    //Create id=>struct mapping
+    mapping(uint256 => Video) public videos;
 
-    //Create Struct
-
-    //Create Event
+    struct Video {
+        uint256 id;
+        string hash;
+        string title;
+        address author;
+    }
 
     constructor() public {}
+
+    //Create Event to listen when video is uplaoded
+    event VideoUploaded(uint256 id, string hash, string title, address author);
 
     function uploadVideo(string memory _videoHash, string memory _title)
         public
     {
         // Make sure the video hash exists
+        require(bytes(_videoHash).length > 0);
+
         // Make sure video title exists
+        require(bytes(_title).length > 0);
+
         // Make sure uploader address exists
+        require(msg.sender != address(0));
+
         // Increment video id
+        videoCount++;
+
         // Add video to the contract
+        videos[videoCount] = Video(videoCount, _videoHash, _title, msg.sender);
+
         // Trigger an event
+        emit VideoUploaded(videoCount, _videoHash, _title, msg.sender);
     }
 }
